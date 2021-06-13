1b8a38f71236027b497af06924a87d2815230299
Bruno Fialho <brugerf@gmail.com>
2021-06-13 11:05:42 -0300

    Add React Hook Form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Input } from '../components/Form/input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(values);
+  }
+  
+  // const { errors } = formState;
+
   return (
     <Flex
       w="100vw"
@@ -17,10 +33,23 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input type="email" name="email" label="E-mail" />
-          <Input type="password" name="password" label="Senha" />
+          <Input
+            type="email"
+            name="email"
+            label="E-mail"
+            // error={errors.email}
+            {...register('email')}
+          />
+          <Input
+            type="password"
+            name="password"
+            label="Senha"
+            // error={errors.password}
+            {...register('password')}
+          />
         </Stack>
 
         <Button
@@ -28,6 +57,7 @@ export default function SignIn() {
           mt="6"
           colorScheme="pink"
           size="lg"
+          isLoading={formState.isSubmitting}
         >
           Entrar
         </Button>
