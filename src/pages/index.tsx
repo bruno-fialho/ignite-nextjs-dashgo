0e093ffb7ff41d381891c0f3a668eebc1f7b4165
Bruno Fialho <brugerf@gmail.com>
2021-06-13 11:46:08 -0300

    Add create user form validation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,6 @@ export default function SignIn() {
   });
   const { errors } = formState;
 
-  console.log(errors);
-
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
 
