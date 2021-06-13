f533e159282835a99cf0d7ac5bb95e3acee44ee6
Bruno Fialho <brugerf@gmail.com>
2021-06-13 08:59:17 -0300

    Add responsiveness to header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Bruno Fialho</Text>
-        <Text color="gray.300" fontSize="small">
-          bruno@mail.com
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Bruno Fialho</Text>
+          <Text color="gray.300" fontSize="small">
+            bruno@mail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Bruno Fialho" src="https://github.com/bruno-fialho.png" />
     </Flex>
