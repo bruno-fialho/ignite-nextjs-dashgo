3f12564d1d8cb4820fa97aecd915dafdbaa46dfe
Bruno Fialho <brugerf@gmail.com>
2021-10-16 11:12:36 -0300

    Add: Pagination at mirageJS

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
 
-import { useQuery } from 'react-query';
-
 import { 
   Box,
   Flex,
@@ -29,29 +27,10 @@ import Header from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const { data } = await api.get<any>('users');
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5, // 5 seconds
-  });
+  const { data, isLoading, isFetching, error } = useUsers();
 
   console.log(data);
 
